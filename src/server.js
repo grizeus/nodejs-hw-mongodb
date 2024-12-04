02a9b3133a8cf7191a052c1ece2dd0a02b06b921
grizeus <zaytsewww@gmail.com>
2024-12-04 04:03:17 +0200

    add logic to server app

--- src/server.js
+++ src/server.js
@@ -1,1 +1,46 @@
-console.log("Hello world");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import pino from "pino-http";
+
+dotenv.config();
+
+const PORT = parseInt(process.env.PORT) || 3000;
+
+export const startServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: "pino-pretty",
+      },
+    }),
+  );
+
+  app.get("/", (req, res) => {
+    res.json({
+      message: "Hello world!",
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: "Something went wrong",
+      error: err.message,
+    });
+  });
+
+  app.use("*", (req, res, next) => {
+    res.status(404).json({
+      message: "Not found",
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
