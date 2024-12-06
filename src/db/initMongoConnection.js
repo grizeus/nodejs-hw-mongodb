f390247267927392f80fc57dbbf28fc766f65b74
grizeus <zaytsewww@gmail.com>
2024-12-06 19:00:10 +0200

    .

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -3,10 +3,19 @@ import mongoose from "mongoose";
 
 dotenv.config();
 export const initMongoDB = async () => {
-    try {
-      console.log('Mongo connection successfully established!');
-    } catch (e) {
-      console.log('Error while setting up mongo connection', e);
-      throw e;
-    }
-  };
+  try {
+    const url = process.env.MONGODB_URL;
+    const db = process.env.MONGODB_DB;
+    const user = process.env.MONGODB_USER;
+    const pwd = process.env.MONGODB_PASSWORD;
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
+    );
+    console.log("Mongo connection successfully established!");
+  } catch (e) {
+    console.log("Error while setting up mongo connection", e);
+    throw e;
+  }
+};
+
+await initMongoDB();
