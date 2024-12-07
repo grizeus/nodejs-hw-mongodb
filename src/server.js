cc04ebc106b058e3d07ef7c4c46262aecd533bff
grizeus <zaytsewww@gmail.com>
2024-12-07 15:54:21 +0200

    add routers

--- src/server.js
+++ src/server.js
@@ -2,10 +2,10 @@ import express from "express";
 import cors from "cors";
 import pino from "pino-http";
 
+import contactsRouter from "./routers/contacts.js";
 import { getEnvVar } from "./utils/getEnvVar.js";
-import { getAllContacts, getContactById } from "./services/contacts.js";
 
-const PORT = getEnvVar("PORT", 3000);
+const PORT = Number(getEnvVar("PORT", 3000));
 
 export const setupServer = () => {
   const app = express();
@@ -20,31 +20,10 @@ export const setupServer = () => {
       },
     }),
   );
-
-  app.get("/contacts", async (req, res) => {
-    const contacts = await getAllContacts();
-    res.status(200).json({
-      status: 200,
-      message: "Contacts retrieved successfully",
-      data: contacts,
-    });
-  });
-
-  app.get("/contacts/:contactId", async (req, res) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-
-    if (!contact) {
-      res.status(404).json({
-        message: "Contact not found",
-      });
-      return;
-    }
-
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
+  app.use(contactsRouter);
+  app.use("*", (req, res, next) => {
+    res.status(404).json({
+      message: "Not found",
     });
   });
 
@@ -52,12 +31,6 @@ export const setupServer = () => {
     res.status(500).json({
       message: "Something went wrong",
       error: err.message,
-    });
-  });
-
-  app.use("*", (req, res, next) => {
-    res.status(404).json({
-      message: "Not found",
     });
   });
 
