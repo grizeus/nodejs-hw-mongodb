f6a4002f841fc13435ec38dcc28e3be86775b4c6
grizeus <zaytsewww@gmail.com>
2024-12-07 14:47:22 +0200

    fix type in return data

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ export const setupServer = () => {
   app.get("/contacts", async (req, res) => {
     const contacts = await getAllContacts();
     res.status(200).json({
-      status: "200",
+      status: 200,
       message: "Contacts retrieved successfully",
       data: contacts,
     });
@@ -42,7 +42,7 @@ export const setupServer = () => {
     }
 
     res.status(200).json({
-      status: "200",
+      status: 200,
       message: `Successfully found contact with id ${contactId}!`,
       data: contact,
     });
