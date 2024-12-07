09d3682369444f2cddb7804f860cf44be20f5147
grizeus <32486511+grizeus@users.noreply.github.com>
2024-12-08 00:05:13 +0200

    Create post route for contact (#10)

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,6 +1,10 @@
 import createHttpError from "http-errors";
 
-import { getAllContacts, getContactById } from "../services/contacts.js";
+import {
+  createContact,
+  getAllContacts,
+  getContactById,
+} from "../services/contacts.js";
 
 export const getContactsController = async (req, res) => {
   const contacts = await getAllContacts();
@@ -16,12 +20,22 @@ export const getContactByIdController = async (req, res, next) => {
   const contact = await getContactById(contactId);
 
   if (!contact) {
-    throw createHttpError(404, 'Contact not found');
+    throw createHttpError(404, "Contact not found");
   }
 
   res.status(200).json({
     status: 200,
     message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+};
+
+export const createContactController = async (req, res) => {
+  const contact = await createContact(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: "Succesfully created a contact!",
     data: contact,
   });
 };
--- src/server.js
+++ src/server.js
@@ -13,7 +13,12 @@ export const setupServer = () => {
   const app = express();
 
   app.use(cors());
-  app.use(express.json());
+  app.use(
+    express.json({
+      type: "application/json",
+      limit: "1kb",
+    }),
+  );
 
   app.use(
     pino({
