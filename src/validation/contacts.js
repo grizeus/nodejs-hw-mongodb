be0c51abef40a56357f719f50e5d69721bf941b9
grizeus <zaytsewww@gmail.com>
2024-12-08 10:37:01 +0200

    .

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -5,5 +5,5 @@ export const createContactSchema = Joi.object({
   phoneNumber: Joi.string().min(3).max(30).required(),
   email: Joi.string().email(),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string().valid("work", "personal", "home").required,
+  contactType: Joi.string().valid("work", "personal", "home").required(),
 });
