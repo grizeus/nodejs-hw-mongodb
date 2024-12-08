7d1f5174a60e1b8a269b6a443e849a634d24c308
grizeus <zaytsewww@gmail.com>
2024-12-08 09:55:55 +0200

    .

---
+++ src/validation/contacts.js
@@ -0,0 +1,9 @@
+import Joi from "joi";
+
+export const createContactSchema = Joi.object({
+  name: Joi.string().min(2).max(30).required(),
+  phoneNumber: Joi.string().min(3).max(30).required(),
+  email: Joi.string().email(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid("work", "personal", "home").required,
+});
