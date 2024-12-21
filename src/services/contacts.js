d4099a4f768fec35e43625692b3a465ad6c90c4e
grizeus <zaytsewww@gmail.com>
2024-12-21 22:52:51 +0200

    change SORT_ORDER struct type

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -5,7 +5,7 @@ import { calculatePaginationData } from "../utils/calculatePaginationData.js";
 export const getAllContacts = async ({
   page = 1,
   perPage = 10,
-  sortOrder = SORT_ORDER.ASC,
+  sortOrder = SORT_ORDER[0],
   sortBy = "_id",
   filter = {},
 }) => {
