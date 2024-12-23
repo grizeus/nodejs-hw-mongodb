e1f23fff93f6f26e4b21753846d1544263f58b25
grizeus <zaytsewww@gmail.com>
2024-12-23 16:05:28 +0200

    .

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import pino from "pino-http";
 
-import contactsRouter from "./routers/contacts.js";
+import router from "./routers/index.js";
 import { getEnvVar } from "./utils/getEnvVar.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import { notFoundHandler } from "./middlewares/notFoundHandler.js";
@@ -28,7 +28,7 @@ export const setupServer = () => {
     }),
   );
 
-  app.use(contactsRouter);
+  app.use(router);
   app.use("*", notFoundHandler);
   app.use(errorHandler);
 
