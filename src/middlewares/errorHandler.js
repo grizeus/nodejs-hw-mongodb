76592901270b20b48072b7c85461e3a7c0b968bd
grizeus <zaytsewww@gmail.com>
2024-12-25 01:17:46 +0200

    Fix error handling middleware

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,8 +1,18 @@
-export const errorHandler = (error, req, res, next) => {
-  const { status = 500, message } = error;
+import { HttpError } from "http-errors";
+
+export const errorHandler = (err, req, res, next) => {
+  if (err instanceof HttpError) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.name,
+      data: err,
+    });
+    return;
+  }
+
+  const { status = 500, message } = err;
   res.status(status).json({
     status,
     message,
   });
 };
-
