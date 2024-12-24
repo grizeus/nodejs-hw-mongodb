f3e895e216ceb13d3836d90b47b35e9449dd6ea1
grizeus <zaytsewww@gmail.com>
2024-12-25 00:50:03 +0200

    Refactor user registration logic and enhance validation schema

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -1,9 +1,11 @@
 import Joi from "joi";
 
+import { emailRegExp } from "../constants/index.js";
+
 export const registerUserSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  email: Joi.string().pattern(emailRegExp).required(),
+  password: Joi.string().min(8).required(),
 });
 
 export const sessionSchema = Joi.object({
