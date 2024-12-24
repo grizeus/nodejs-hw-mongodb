ad277e48cfcc579fc326f282a5f88e68073e28b1
grizeus <32486511+grizeus@users.noreply.github.com>
2024-12-25 01:18:51 +0200

    Fix error handling middleware (#28)

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,14 @@
+import createHttpError from "http-errors";
 import { UsersCollection } from "../db/models/user.js";
 
 export const registerUser = async (payload) => {
+  const { email } = payload;
+  const user = await UsersCollection.findOne({ email });
+
+  if (user) {
+    throw createHttpError(409, "User already exist");
+  }
+
   const newUser = await UsersCollection.create(payload);
   return newUser;
 };
