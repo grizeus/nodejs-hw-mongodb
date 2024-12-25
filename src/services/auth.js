5b3883aa5b4bfacccf23d24770cc197b440155cb
grizeus <zaytsewww@gmail.com>
2024-12-25 18:18:58 +0200

    .

--- src/services/auth.js
+++ src/services/auth.js
@@ -55,3 +55,45 @@ export const loginUser = async ({ email, password }) => {
 export const logoutUser = async (sessionId) => {
   await SessionCollection.deleteOne({ _id: sessionId });
 };
+
+const createSession = () => {
+  const accessToken = randomBytes(30).toString("base64");
+  const refreshToken = randomBytes(30).toString("base64");
+
+  return {
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil: Date.now() + accessTokenLifetime,
+    refreshTokenValidUntil: Date.now() + refreshTokenLifetime,
+  };
+};
+
+export const refreshUsersSession = async ({ sessionId, refreshToken }) => {
+  const session = await SessionCollection.findOne({
+    _id: sessionId,
+    refreshToken,
+  });
+
+  if (!session) {
+    throw createHttpError(401, "Session not found");
+  }
+
+  const isSessionTokenExpired =
+    new Date() > new Date(session.refreshTokenValidUntil);
+
+  if (isSessionTokenExpired) {
+    throw createHttpError(401, "Session token expired");
+  }
+
+  const newSession = createSession();
+
+  await SessionCollection.deleteOne({
+    _id: sessionId,
+    refreshToken,
+  });
+
+  return await SessionCollection.create({
+    userId: session.userId,
+    ...newSession,
+  });
+};
