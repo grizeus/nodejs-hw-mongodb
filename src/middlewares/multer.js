8d03eb5d0560a58c6224833d52d3478a599cce89
grizeus <zaytsewww@gmail.com>
2025-01-10 21:22:46 +0200

    add new features to multer config

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -1,14 +1,27 @@
 import multer from "multer";
 import { TEMP_UPLOAD_DIR } from "../constants/index.js";
+import createHttpError from "http-errors";
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: (req, file, cb) => {
     cb(null, TEMP_UPLOAD_DIR);
   },
-  filename: function (req, file, cb) {
-    const uniquePrefix = Date.now();
+  filename: (req, file, cb) => {
+    const uniquePrefix = `${Date.now()}_${Math.round(Math.random() * 1E9)}`;
     cb(null, `${uniquePrefix}_${file.originalname}`);
   },
 });
 
-export const upload = multer({ storage });
+const limits = {
+  fileSize: 1024 * 1024 * 5,
+};
+
+const fileFilter = (req, file, cb) => {
+  const ext = file.originalname.split(".").pop();
+  if (ext === "exe" || ext === "EXE") {
+    cb(createHttpError(400, "Files with .exe extention not allowed"));
+  }
+  cb(null, true);
+};
+
+export const upload = multer({ storage, limits, fileFilter });
